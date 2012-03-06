a7523d96f744b67fa4894bff721e40cb05c53cf1
firegroundassist <firegroundassist@gmail.com>
2012-03-06 00:21:36 -0500

    Created FGSideInfoView class

---
+++ FireGroundAssist/Fire Ground Assist/FGSideInfoView.h
@@ -0,0 +1,15 @@
+//
+//  FGSideInfoView.h
+//  Fire Ground Assist
+//
+//  Created by Charlie Jacobson on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FGSideInfoView : UIView {
+	
+}
+
+@end
---
+++ FireGroundAssist/Fire Ground Assist/FGSideInfoView.m
@@ -0,0 +1,13 @@
+//
+//  FGSideInfoView.m
+//  Fire Ground Assist
+//
+//  Created by Charlie Jacobson on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FGSideInfoView.h"
+
+@implementation FGSideInfoView
+
+@end
