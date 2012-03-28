b6ffdc2294cbd2326274b9c482125bfdaf1dd012
firegroundassist <firegroundassist@gmail.com>
2012-03-28 11:54:14 -0400

    added expanded fields, animation for expanding/collapsing

--- FireGroundAssist/Fire Ground Assist/FGContactView.h
+++ FireGroundAssist/Fire Ground Assist/FGContactView.h
@@ -12,12 +12,21 @@
 
 @property (nonatomic) BOOL isExpanded; 
 
+// main fields
 @property (retain, nonatomic) UILabel* position;
 @property (retain, nonatomic) UILabel* name;
 @property (retain, nonatomic) UILabel* mainNumberType;
 @property (retain, nonatomic) UITextView* mainNumber; 
  
+// expanded fields
+@property (retain, nonatomic) UILabel* secondNumberType;
+@property (retain, nonatomic) UITextView* secondNumber;
+@property (retain, nonatomic) UITextView* email;
+@property (retain, nonatomic) UILabel* emailLabel;
+
 
 - (NSInteger) expandOrCollapseView;
+- (void) updateData: (NSDictionary*)data;
+- (void) clearData;
 
 @end
--- FireGroundAssist/Fire Ground Assist/FGContactView.m
+++ FireGroundAssist/Fire Ground Assist/FGContactView.m
@@ -10,7 +10,7 @@
 
 @implementation FGContactView
 
-@synthesize isExpanded, position, name, mainNumber, mainNumberType; 
+@synthesize isExpanded, position, name, mainNumber, mainNumberType, secondNumber, secondNumberType, email, emailLabel; 
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -22,12 +22,63 @@
 		// Position
 		position = [[UILabel alloc] initWithFrame:CGRectMake(5, 5, (self.frame.size.width-15)/2, 20)];
 		position.textColor = [UIColor whiteColor];
-		position.text = @"Engineer"; 
+		position.text = @"Owner"; 
 		position.backgroundColor = [UIColor clearColor];
 		[self addSubview:position];
 		
 		
-		// 
+		// Name
+		name = [[UILabel alloc] initWithFrame:CGRectMake((self.frame.size.width-15)/2+10, 5, (self.frame.size.width-15)/2, 20)];
+		name.textColor = [UIColor whiteColor];
+		name.text = @"Charlie Jacobson";
+		name.backgroundColor = [UIColor clearColor];
+		[self addSubview:name];
+		
+		// Main Number Type
+		mainNumberType = [[UILabel alloc] initWithFrame:CGRectMake(5, 25, (self.frame.size.width-15)/3, 20)];
+		mainNumberType.textColor = [UIColor whiteColor];
+		mainNumberType.text = @"Cell";
+		mainNumberType.backgroundColor = [UIColor blueColor];
+		[self addSubview:mainNumberType];
+		
+		// Main Number
+		mainNumber = [[UITextView alloc] initWithFrame:CGRectMake((self.frame.size.width-15)/3+10, 25, 2*(self.frame.size.width-15)/3, 20)];
+		mainNumber.textColor = [UIColor whiteColor];
+		mainNumber.text = @"201.290.2454";
+		mainNumber.backgroundColor = [UIColor blueColor];
+		[self addSubview:mainNumber];
+		
+		// Second Number Type
+		secondNumberType = [[UILabel alloc] initWithFrame:CGRectMake(5, 50, (self.frame.size.width-15)/3, 20)];
+		secondNumberType.textColor = [UIColor whiteColor];
+		secondNumberType.text = @"Home";
+		secondNumberType.backgroundColor = [UIColor blueColor];
+		secondNumberType.hidden = TRUE;
+		[self addSubview:secondNumberType];
+		
+		// Second Number
+		secondNumber = [[UITextView alloc] initWithFrame:CGRectMake((self.frame.size.width-15)/3+10, 50, 2*(self.frame.size.width-15)/3, 20)];
+		secondNumber.textColor = [UIColor whiteColor];
+		secondNumber.text = @"201.227.0666";
+		secondNumber.backgroundColor = [UIColor blueColor];
+		secondNumber.hidden = TRUE;
+		[self addSubview:secondNumber];
+		
+		// Email Label
+		emailLabel = [[UILabel alloc] initWithFrame:CGRectMake(5, 75, (self.frame.size.width-15)/3, 20)];
+		emailLabel.textColor = [UIColor whiteColor];
+		emailLabel.text = @"Email";
+		emailLabel.backgroundColor = [UIColor blueColor];
+		emailLabel.hidden = TRUE;
+		[self addSubview:emailLabel];
+		
+		// Email
+		email = [[UITextView alloc] initWithFrame:CGRectMake((self.frame.size.width-15)/3+10, 75, 2*(self.frame.size.width-15)/3, 20)];
+		email.textColor = [UIColor whiteColor];
+		email.text = @"jacobc@d-e.org";
+		email.backgroundColor = [UIColor blueColor];
+		email.hidden = TRUE;
+		[self addSubview:email];
 		
     }
     return self;
@@ -41,12 +92,28 @@
 		CGRect frame = self.frame;
 		frame.size.height = frame.size.height + 100.0f;
 		
-		[UIView beginAnimations:nil context:nil];
-		[UIView setAnimationDuration:0.4f];
-		self.frame = frame;
+		
+		
+		[UIView animateWithDuration:.5 
+							  delay:0
+							options:UIViewAnimationCurveEaseIn
+						 animations:^{
+							 
+							 self.frame = frame;}
+						 completion:^(BOOL finished){
+							 
+							 self.isExpanded = TRUE;
+						 }
+		 ];
 		[UIView commitAnimations];
 		
-		self.isExpanded = TRUE;
+		[self setNeedsDisplay];
+		
+		secondNumberType.hidden = FALSE;
+		secondNumber.hidden = FALSE;
+		emailLabel.hidden = FALSE;
+		email.hidden = FALSE;
+		
 		return changeY;
 	}
 	
@@ -56,17 +123,38 @@
 		CGRect frame = self.frame;
 		frame.size.height = frame.size.height - 100.0f;
 		
-		[UIView beginAnimations:nil context:nil];
-		[UIView setAnimationDuration:0.4f];
-		self.frame = frame;
+		[UIView animateWithDuration:.5 
+							  delay:0
+							options:UIViewAnimationCurveEaseOut
+						 animations:^{
+							 
+							 self.frame = frame;
+						 }completion:^(BOOL finished){
+							 
+							 self.isExpanded = FALSE;
+						 }
+		 ];
+		secondNumberType.hidden = TRUE;
+		secondNumber.hidden = TRUE;
+		emailLabel.hidden = TRUE;
+		email.hidden = TRUE;
+		
 		[UIView commitAnimations];
 		
-		self.isExpanded = FALSE;
+		
 		return changeY;
 	}
 }
 
+- (void)updateData:(NSDictionary *)data
+{
+	
+}
 
+- (void)clearData
+{
+	
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
